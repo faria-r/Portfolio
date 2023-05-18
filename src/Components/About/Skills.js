a6e458ac9692892a507ecd46719671458912a710
faria-r <fariacse11@gmail.com>
2023-05-18 21:43:37 +0600

    img added in skills

--- src/Components/About/Skills.js
+++ src/Components/About/Skills.js
@@ -1,13 +1,33 @@
 import React from 'react';
-
+import img1 from '../../assets/img/js.jpg'
+import img2 from '../../assets/img/html.png'
 const Skills = () => {
     return (
         <div>
         <div className="card  h-48 w-96 bg-base-100 shadow-xl mt-24 image-full -rotate-[19deg]">
 
-<div className="card-body">
-<h2 className="card-title justify-end">Front-end Developer</h2>
-<p className='text-2xl text-green-700'>Faria Sultana Bintha Rohman</p>
+<div className="card-body grid grid-cols-4 gap-3">
+<div className='rounded-full'>
+    <img src={img1} className='w-[48px]'></img>
+</div>
+<div className='rounded-full'>
+    <img src={img1} className='w-[48px]'></img>
+</div>
+<div className='rounded-full'>
+    <img src={img1} className='w-[48px]'></img>
+</div>
+<div className='rounded-full'>
+    <img src={img1} className='w-[48px]'></img>
+</div>
+<div className='rounded-full'>
+    <img src={img1} className='w-[48px]'></img>
+</div>
+<div className='rounded-full'>
+    <img src={img1} className='w-[48px]'></img>
+</div>
+<div className='rounded-full'>
+    <img src={img1} className='w-[48px]'></img>
+</div>
 
 </div>
 </div>
