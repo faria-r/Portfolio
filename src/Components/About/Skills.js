5c0f2ea7b7a8bfd3a8e936b1c85031e2fc2f10bb
faria-r <fariacse11@gmail.com>
2023-05-19 21:32:14 +0600

    animation added in skills

--- src/Components/About/Skills.js
+++ src/Components/About/Skills.js
@@ -1,6 +1,13 @@
 import React from 'react';
-import img1 from '../../assets/img/js.jpg'
+import img1 from '../../assets/img/js.png'
 import img2 from '../../assets/img/html.png'
+import img3 from '../../assets/img/M.png'
+import img4 from '../../assets/img/c.png'
+import img5 from '../../assets/img/n.png'
+import img6 from '../../assets/img/e.png'
+import img7 from '../../assets/img/logo192.png'
+import img8 from '../../assets/img/t.png'
+import style from './style/animation.css'
 const Skills = () => {
     return (
         <div>
@@ -8,25 +15,28 @@ const Skills = () => {
 
 <div className="card-body grid grid-cols-4 gap-3">
 <div className='rounded-full'>
-    <img src={img1} className='w-[48px]'></img>
+    <img src={img7} className='w-[48px] spiner'></img>
 </div>
 <div className='rounded-full'>
-    <img src={img1} className='w-[48px]'></img>
+    <img src={img3} className='w-[48px] spiner'></img>
 </div>
 <div className='rounded-full'>
-    <img src={img1} className='w-[48px]'></img>
+    <img src={img5} className='w-[64px] spiner'></img>
 </div>
 <div className='rounded-full'>
-    <img src={img1} className='w-[48px]'></img>
+    <img src={img6} className='w-[52px] spiner'></img>
 </div>
 <div className='rounded-full'>
-    <img src={img1} className='w-[48px]'></img>
+    <img src={img8} className='w-[48px] spiner'></img>
 </div>
 <div className='rounded-full'>
-    <img src={img1} className='w-[48px]'></img>
+    <img src={img1} className='w-[85px] spiner'></img>
 </div>
 <div className='rounded-full'>
-    <img src={img1} className='w-[48px]'></img>
+    <img src={img4} className='w-[48px] spiner'></img>
+</div>
+<div className='rounded-full'>
+    <img src={img2} className='w-[48px] spiner'></img>
 </div>
 
 </div>
