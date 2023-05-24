4f41976e705cf2c4a19a4e45202449493d4d733e
faria-r <fariacse11@gmail.com>
2023-05-25 00:28:36 +0600

    card color updated

--- src/Components/About/Cards.js
+++ src/Components/About/Cards.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Cards = () => {
     return (
         <div>
-            <div className="card  lg:h-48 sm:w-[80vw] lg:w-96 bg-lime-400 shadow-xl image-full sm:-rotate-[15deg] sm:mt-16 lg:-rotate-[19deg]">
+            <div className="card  lg:h-48 sm:w-[80vw] lg:w-96  shadow-xl image-full sm:-rotate-[15deg] sm:mt-16 lg:-rotate-[19deg]">
 
   <div className="card-body">
     <h2 className="card-title justify-end">Front-end Developer</h2>
