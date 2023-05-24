5053c8464a7a87a275dc85273e8d48d2f6f39e82
faria-r <fariacse11@gmail.com>
2023-05-25 00:20:30 +0600

    color changed in social icon

--- src/Components/SocialLInks/Social.js
+++ src/Components/SocialLInks/Social.js
@@ -6,16 +6,16 @@ const Social = () => {
         <div className=' hidden  lg:block'>
             <div className="lg:left-0">
         <ul className="steps steps-vertical ">
-          <li className="step ">
+          <li className="step step-success ">
           <a href="https://github.com/faria-r" target='_blank' rel='noreferrer'>  <FaGithub className="text-2xl text-green-600"></FaGithub></a>
           </li>
-          <li className="step ">
+          <li className="step step-success">
           <a href="https://www.linkedin.com/in/faria-r/" target='_blank' rel='noreferrer'>  <FaLinkedin className="text-2xl text-green-600"></FaLinkedin></a>  
           </li>
-          <li className="step ">
+          <li className="step step-success">
           <a href="https://web.facebook.com/faria.frb/" target='_blank' rel='noreferrer'>  <FaFacebook className="text-2xl text-green-600"></FaFacebook></a> 
           </li>
-          <li className="step">
+          <li className="step step-success">
           <a href="https://twitter.com/FariaR20" target='_blank' rel='noreferrer'>  <FaTwitter className="text-2xl text-green-600"></FaTwitter></a> 
           </li>
         </ul>
