61d726ad73427c39db79b51a2de2bfb65e3d0d44
faria-r <fariacse11@gmail.com>
2023-05-25 09:27:04 +0600

    Email added in right layout

--- src/Components/Layouts/Main.js
+++ src/Components/Layouts/Main.js
@@ -3,15 +3,20 @@ import { Outlet } from 'react-router-dom';
 import Footer from '../Home/Footer/Footer';
 import Navbar from '../Home/navbar/Navbar';
 import Social from '../SocialLInks/Social';
+import Email from '../SocialLInks/Email';
 
 const Main = () => {
     return (
-        <div>
+        <div className='w-full'>
             <Navbar></Navbar>
-           <div>
+           <div >
+           <div className='lg:fixed bottom-0 right-4'>
+<Email></Email>
+            </div>
             <div className='lg:fixed mt-0 bottom-0'>
 <Social></Social>
             </div>
+           
             <Outlet></Outlet>
            </div>
            <Footer></Footer>
---
+++ src/Components/SocialLInks/Email.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import style from './Styles.css'
+const Email = () => {
+    return (
+        <div className='hidden  lg:block text-Slate'>
+            <div className='lg:right-8 '>
+           <div className='flex flex-col-reverse justify-center items-center '>
+           <div className='h-24 border-r-2 w-[1px] bg-green-600 border-Slate'></div>
+            <div>
+                <p className='textRotation mb-6'>Fariarahman622@gmail.com</p>
+            </div>
+           </div>
+           
+        
+            
+         
+        
+            </div>
+        </div>
+    );
+};
+
+export default Email;
